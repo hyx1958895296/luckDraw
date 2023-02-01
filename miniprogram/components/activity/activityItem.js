bbc4c9ea84d6fe9e2168587fcd1287b795e7b099
zhujiangyan <t2710086269@163.com>
2023-02-01 09:42:45 +0800

    acyivity

---
+++ miniprogram/components/activity/activityItem.js
@@ -0,0 +1,24 @@
+// components/activity/activityItem.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  // 父组件的参数
+  properties: {
+    activity:Object
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
