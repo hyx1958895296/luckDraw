85389d05b74b1763cc7b493245e840a87172ba8d
shangyuhao <834725611@qq.com>
2023-02-06 21:16:00 +0800

    添加了用户信息接口 跟登录的一些业务逻辑

--- miniprogram/pages/commonTopics/commonTopics.js
+++ miniprogram/pages/commonTopics/commonTopics.js
@@ -5,7 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    localShowImage: [],
+    imageList :[],
+    msg:"",
+    phoneValue:""
   },
 
   /**
@@ -13,6 +16,75 @@ Page({
    */
   onLoad(options) {
 
+  },
+
+  setMsg(e){
+    this.setData({
+      msg:e.detail.value
+    })
+  },
+
+  setPhoneInput(e){
+    this.setData({
+      phoneValue:e.detail.value
+    })
+  },
+
+  upImage() {
+    let _this = this;
+    let   arr     = [];
+    wx.chooseMedia({
+      count: 5,
+      success(res) {
+        _this.setData({
+          localShowImage: res.tempFiles
+        })
+        res.tempFiles.forEach(image => {
+          let po = image.tempFilePath.lastIndexOf(".");
+          let ext = image.tempFilePath.slice(po);
+          wx.cloud.uploadFile({
+            cloudPath: new Date().getTime() + ext,
+            filePath:  image.tempFilePath,
+            success(res) {
+               if(!res.fileID) return;
+               arr.push(res.fileID)
+               _this.setData({
+                 imageList:arr
+               })
+            }
+          })
+        });
+      }
+    })
+  },
+
+  submit(){
+     wx.cloud.callFunction({
+       name:"feedBack",
+       data:{
+         type:"create",
+         feedBackInfo:{
+          comments:this.data.msg,
+          phone:this.data.phoneValue,
+          image:this.data.imageList
+         }
+       },
+       success(res){
+          if(res.result.status==1){
+            wx.showToast({
+              title: res.result.msg,
+              icon:"none",
+              duration:2000
+            })
+          }else{
+            wx.showToast({
+              title: res.result.msg,
+              icon:"none",
+              duration:2000
+            })
+          }
+       }
+     })
   },
 
   /**
