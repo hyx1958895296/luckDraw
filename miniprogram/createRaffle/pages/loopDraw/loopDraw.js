16787fc16629a057bccc3adf65e8715f9b8ea886
zhujiangyan <t2710086269@163.com>
2023-02-09 09:21:03 +0800

    创建了管理循环抽奖期数的页面

--- miniprogram/createRaffle/pages/loopDraw/loopDraw.js
+++ miniprogram/createRaffle/pages/loopDraw/loopDraw.js
@@ -43,7 +43,32 @@ Page({
         active: false,
         title: '全选',
       },
-    ]
+    ],
+    activeText: '',
+    activeArr: [],
+    endDate: ''
+  },
+
+  // 计算循环抽奖结束时间
+  endDate() {
+    let num = this.data.activeArr.length * this.data.multiIndexCount;
+    console.log(num);
+    let nowDate = new Date();
+    let dateArr = [];
+    let yueRi = '';
+    dateArr.push(nowDate.getMonth() + 1 + '月' + nowDate.getDate() + '日')
+    for (let i = 0; i < num; i++) {
+      let tempDate = nowDate.setDate(nowDate.getDate() + 1);
+      tempDate = new Date(tempDate);
+      let ri = tempDate.getDate();
+      let yue = tempDate.getMonth() + 1;
+      let nian = tempDate.getFullYear();
+      yueRi = nian + '年' + yue + '月' + ri + "日";
+    }
+    console.log(yueRi);
+    this.setData({
+      endDate:yueRi
+    })
   },
 
   // 弹窗
@@ -61,49 +86,142 @@ Page({
   // 选择item
   active(e) {
     let index = e.target.dataset.item;
-    let item =  this.data.dateList[index];
+    let item = this.data.dateList[index];
+    let arr = [];
     let allIndex = 0;
-    this.data.dateList.forEach((item,index)=> {
-      if(item.title == "全选"){
-        allIndex= index;
+    this.data.dateList.forEach((item, index) => {
+      if (item.title == "全选") {
+        allIndex = index;
       }
     });
-    if(item.title != "全选"){
-      if(item.active){
+    if (item.title != "全选") {
+      arr = [item.title]
+      if (item.active) {
         this.setData({
-          [`dateList[${index}].active`] : false,
-          [`dateList[${allIndex}].active`] : false,
+          [`dateList[${index}].active`]: false,
+          [`dateList[${allIndex}].active`]: false,
+          activeArr: this.data.activeArr.filter(item => item != arr[0])
         })
-      }else{
+      } else {
         this.setData({
-          [`dateList[${index}].active`] : true
+          [`dateList[${index}].active`]: true,
+          activeArr: this.data.activeArr.concat(arr)
         })
-        let is = this.data.dateList.find(item=> item.active == false && item.title!= '全选');
-        if(is == undefined){
+        let is = this.data.dateList.find(item => item.active == false && item.title != '全选');
+        if (is == undefined) {
           this.setData({
-            [`dateList[${allIndex}].active`] : true,
+            [`dateList[${allIndex}].active`]: true,
           })
         }
       }
-    }else{
-       if(item.active){
-        this.data.dateList.forEach((item,index) => {
+    } else {
+      if (item.active) {
+        arr = []
+        this.data.dateList.forEach((item, index) => {
           this.setData({
-            [`dateList[${index}].active`] : false
+            [`dateList[${index}].active`]: false,
+            activeArr: []
           })
         });
-       }else{
-        this.data.dateList.forEach((item,index) => {
+      } else {
+        this.data.dateList.forEach(item => {
+          arr.push(item.title)
+        });
+        this.data.dateList.forEach((item, index) => {
           this.setData({
-            [`dateList[${index}].active`] : true
+            [`dateList[${index}].active`]: true,
+            activeArr: [...new Set(this.data.activeArr.concat(arr))].filter(item => item != '全选'),
           })
-        });  
-       }
+        });
+      }
     }
+    this.setData({
+      activeArr: this.sortWeeks(this.data.activeArr)
+    })
   },
 
-   // 跳转
-   navigateto(e) {
+  // 展示给用户看的
+  getActiveText() {
+    let text = '';
+    if (this.data.activeArr.length == 7) {
+      text = '每周每天';
+    } else {
+      this.data.activeArr.forEach(item => {
+        text += item.slice(2, 3) + '、';
+      })
+      text = '每周' + text;
+    }
+    this.setData({
+      activeText: text.replace(/、$/, '')
+    })
+  },
+
+  // 排序星期
+  sortWeeks(weeks) {
+    var _weeks = []; //创建临时排序的数组
+    for (var i = 0; i < weeks.length; i++) {
+      if (weeks[i] == "每周一") {
+        var _week = {};
+        _week["id"] = 1;
+        _week["name"] = "每周一";
+        _weeks.push(_week);
+      }
+      if (weeks[i] == "每周二") {
+        var _week = {};
+        _week["id"] = 2;
+        _week["name"] = "每周二";
+        _weeks.push(_week);
+      }
+      if (weeks[i] == "每周三") {
+        var _week = {};
+        _week["id"] = 3;
+        _week["name"] = "每周三";
+        _weeks.push(_week);
+      }
+      if (weeks[i] == "每周四") {
+        var _week = {};
+        _week["id"] = 4;
+        _week["name"] = "每周四";
+        _weeks.push(_week);
+      }
+      if (weeks[i] == "每周五") {
+        var _week = {};
+        _week["id"] = 5;
+        _week["name"] = "每周五";
+        _weeks.push(_week);
+      }
+      if (weeks[i] == "每周六") {
+        var _week = {};
+        _week["id"] = 6;
+        _week["name"] = "每周六";
+        _weeks.push(_week);
+      }
+      if (weeks[i] == "每周日") {
+        var _week = {};
+        _week["id"] = 7;
+        _week["name"] = "每周日";
+        _weeks.push(_week);
+      }
+    }
+    _weeks.sort(function (a, b) {
+      return a.id - b.id;
+    });
+    //将weeks清空并将排序好的值赋给weeks
+    weeks = [];
+    for (var i = 0; i < _weeks.length; i++) {
+      weeks.push(_weeks[i].name);
+    }
+    return weeks;
+  },
+
+  keep() {
+    this.hideModal();
+    this.getActiveText();
+
+  },
+
+  // 跳转
+  navigateto(e) {
     wx.navigateTo({
       url: e.currentTarget.dataset.to,
     })
@@ -118,8 +236,9 @@ Page({
   bindMultiPickerChangeCount(e) {
     console.log(e)
     this.setData({
-      multiIndexCount:Number(e.detail.value)+2
-    })
+      multiIndexCount: Number(e.detail.value) + 2
+    });
+    this.endDate()
   },
 
   // 处理时间选择器的数据
@@ -134,15 +253,14 @@ Page({
     }
     let count = [];
     for (let index = 2; index < 29; index++) {
-      count.push(index+'次')    
+      count.push(index + '次')
     }
-    console.log(count)
     this.setData({
       multiArray: [
         hours,
         minutes
       ],
-      multiArrayCount:[
+      multiArrayCount: [
         count
       ]
     })
